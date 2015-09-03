ae7f2192c4c6b3a6d3bfe5619ee018463f1dd521
Evan Widloski <evan@evanw.org>
2015-09-03 15:43:05 -0400

    added gpg key to about

--- about.php
+++ about.php
@@ -1,16 +1,28 @@
 <?php include('header.html')?>
 
 <body>
-	<div class="v vc">
-		<div class="h hc">
-			<div class="simplebox">
-				<div class="title">
-					Evan Widloski
-				</div>
-				<div class="body" style="max-width: 300px;">
-					Trumpeter, sailor and electricity guru in training.  Rarely sees the light of day.  Some say he was raised by dolphins.
-				</div>
-			</div>
-		</div>
-	</div>
+    <div class="v vc">
+        <div class="h hc">
+            <div class="v">
+                <div class="simplebox">
+                    <div class="title">
+                        Evan Widloski
+                    </div>
+                    <div class="body" style="max-width: 300px;">
+                        Trumpeter, sailor and electricity guru in training.  Rarely sees the light of day.  Some say he was raised by dolphins.
+                    </div>
+                </div>
+                <a class="nolink" href="/static/evanwidloski.gpg">
+                <div class="simplebox">
+                    <div class="title">
+                    GPG Key
+                    </div>
+                    <div class="body"> 
+                        Download it here
+                    </div>
+                </div>
+                </a>
+            </div>
+        </div>
+    </div>
 </body>
