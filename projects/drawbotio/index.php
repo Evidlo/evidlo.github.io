a4a59311f9b91e941bc66491844c0f8e0c6bb6c1
Evan Widloski <evan@evanw.org>
2015-09-04 11:19:58 -0400

    added yt vid to drawbotio project

--- projects/drawbotio/index.php
+++ projects/drawbotio/index.php
@@ -6,22 +6,24 @@
 			<div class="simplebox">
 				<div class="title">Drawbotio Drawing Robot</div>
 				<div class="body v">
-            <a class="project_head" href="http://github.com/evidlo/drawbot">
-            <div class="v vc">
-                <div class="h">
-                      <div><img src="/static/github.png"/></div>
-                      <div class="v vc"> Project Files</div>
+                    <a class="project_head" href="http://github.com/evidlo/drawbot">
+                    <div class="v vc">
+                        <div class="h">
+                            <div><img src="/static/github.png"/></div>
+                            <div class="v vc"> Project Files</div>
+                        </div>
+                    </div>
+                    </a>
+                    <div class="annotate">
+                        <img src="stepper.png"/>
+                        Homemade stepper driver from discrete transistors
+                    </div>
+                    This is a wireless drawing robot I built with a friend for the 2014 Boilermake hackathon.  We didn't have a stepper driver at the time, so we built one with some discrete power transistors and a shift register.  Boilermake provided some hackable badges with ATMegas and wireless modules which we repurposed with one badge controlling the robot and another badge acting as a wireless remote. 
+                    <div class="annotate">
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/bDNsi-WeoKM" frameborder="0" allowfullscreen></iframe>
+                    </div>
                 </div>
             </div>
-            </a>
-					<div class="annotate">
-						<img src="stepper.png"/>
-						Homemade stepper driver from discrete transistors
-					</div>
-          This is a wireless drawing robot I built with a friend for the 2014 Boilermake hackathon.  We didn't have a stepper driver at the time, so we built one with some discrete power transistors and a shift register.  Boilermake provided some hackable badges with ATMegas and wireless modules which we repurposed with one badge controlling the robot and another badge acting as a wireless remote. 
-
-				</div>
-			</div>
-		</div>
-	</div>
+        </div>
+    </div>
 </body>
