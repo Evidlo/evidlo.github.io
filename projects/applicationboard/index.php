967aedec660770d138d22fdaf95ce0c44def2eaf
Evan Widloski <evan@evanw.org>
2015-09-05 00:42:31 -0400

    fixed nesting issue

--- projects/applicationboard/index.php
+++ projects/applicationboard/index.php
@@ -17,14 +17,14 @@
 
 					This board was developed for the Purdue ROV team to read sensor data and control various tools.  This way, all the missions specific circuitry is kept on a single board that can be swapped out from year to year and minimize costs.<br><br>
 					Shown above is a test unit to make sure the design worked with our various peripherals.  Below is the final design with the following features:
-					<ul>
-					<li> 5 high power LED drivers for ROV floodlights
-					<li> 2 current-monitored stepper drivers for camera gimbal
-					<li> 2 current-monitored hbridges for a valve turner and gripper
-					<li> 2 servo current monitors for the gripper fingers
-					<li> isolated volt meter (on back)
-					<li> simple on/off control for a pump
-					</p>
+            <ul>
+            <li> 5 high power LED drivers for ROV floodlights
+            <li> 2 current-monitored stepper drivers for camera gimbal
+            <li> 2 current-monitored hbridges for a valve turner and gripper
+            <li> 2 servo current monitors for the gripper fingers
+            <li> isolated volt meter (on back)
+            <li> simple on/off control for a pump
+            </ul>
 
 					<div class="annotate">
 						<img src="appa.png"/>
